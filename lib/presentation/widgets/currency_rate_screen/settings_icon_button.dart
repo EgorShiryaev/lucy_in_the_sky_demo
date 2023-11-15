6ae5643e73a8e8d97cb8604d2f384ad81fc57bfe
EgorShiryaev <shiryaevvyatsu@gmail.com>
2023-11-15 18:38:14 +0300

    Добавлена брокировка иконки настроек

---
+++ lib/presentation/widgets/currency_rate_screen/settings_icon_button.dart
@@ -0,0 +1,26 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/currency_rates_cubit.dart';
+import '../../bloc/currency_rates_states.dart';
+import '../../routes/app_router.gr.dart';
+
+class SettingsIconButton extends StatelessWidget {
+  const SettingsIconButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CurrencyRatesCubit, CurrencyRatesState>(
+      builder: (context, state) {
+        if (state is CurrencyRatesLoadedState) {
+          return IconButton(
+            icon: const Icon(Icons.settings_rounded),
+            onPressed: () => context.router.push(const SettingsRoute()),
+          );
+        }
+        return const SizedBox();
+      },
+    );
+  }
+}
